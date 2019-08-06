66820e7ef27e527d65a0dea71b7e8db9f6f43ed2
James-Livesey <jamesliveg@gmail.com>
2019-08-06 15:38:22 +0100

    Add company player

--- sketches/sketch.js
+++ sketches/sketch.js
@@ -1,24 +1,24 @@
 const testData = {
   "companyA": {
 
-    'sharePrice': [67, 70, 76, 56, 45, 40, 32, 25],
-    'volume': [45, 50, 56, 60, 62, 62, 61, 58]
+    'sharePrice': [67, 70, 43, 56, 45, 40, 45, 25],
+    'volume': [6, 6, 5, 6, 7, 6, 6, 6]
   },
 
   "companyB": {
 
-    'sharePrice': [67, 70, 76, 56, 45, 40, 32, 25],
-    'volume': [45, 50, 56, 60, 62, 62, 61, 58]
+    'sharePrice': [67, 56, 76, 34, 45, 76, 32, 25],
+    'volume': [6, 6, 5, 6, 6, 6, 5, 6]
   },
 
   "companyC": {
-    'sharePrice': [67, 70, 76, 56, 45, 40, 32, 25],
-    'volume': [45, 50, 56, 60, 62, 62, 61, 58]
+    'sharePrice': [67, 70, 54, 56, 54, 40, 23, 25],
+    'volume': [6, 7, 5, 6, 6, 6, 6, 6]
   },
 
   "companyD": {
-    'sharePrice': [67, 70, 76, 56, 45, 40, 32, 25],
-    'volume': [45, 50, 56, 60, 62, 62, 61, 58]
+    'sharePrice': [64, 34, 76, 34, 45, 40, 32, 65],
+    'volume': [6, 6, 6, 6, 7, 6, 6, 5]
   },
 
   "ukInterestRate": [0.67, 0.70, 0.76, 0.56, 0.45, 0.40, 0.32, 0.25],
@@ -30,48 +30,67 @@ const testData = {
 const dummyArray = [60, 0.8];
 
 class Company {
-  constructor() {
-    var synth = new Tone.Synth().toMaster();
+  constructor(name) {
+    this.companyName = name;
+    this.synth = new Tone.Synth().toMaster();
+    this.currentValues = [];
   }
 
-  makeNoteValues(company, index) {
-    var volume = Object.values(testData)[company].volume[index]
-    var sharePrice = Object.values(testData)[company].sharePrice[index]
+  makeNoteValues(index) {
+    var volume = testData[this.companyName].volume[index];
+    var sharePrice = testData[this.companyName].sharePrice[index];
 
-    var currentValues = [volume, sharePrice]
-
-    return currentValues
+    this.currentValues = [sharePrice, volume];
   }
 
   playNote() {
-    synth.volume.value = 6;
-    synth.triggerAttackRelease(Tone.Frequency(60, "midi").toNote(), "8n");
+    this.synth.volume.value = this.currentValues[1];
+    this.synth.triggerAttackRelease(Tone.Frequency(this.currentValues[0], "midi").toNote(), "8n");
+  }
+
+  makeAndPlay(beat) {
+    this.makeNoteValues(beat);
+    this.playNote();
   }
 }
 
+var beat = 0;
+var companies = [];
+var maxBeats = 0;
 
-
-
-
-
-
-
-
-
-
-
-function setup() {
-  // create a canvas
-  createCanvas(600, 800);
-
-  console.log(makeNoteValues(1, 0))
+for (var i = 0; i < Object.keys(testData).length - 2; i++) {
+  companies.push(new Company(Object.keys(testData)[i]));
 }
 
-function draw() {
-  // colour the background black
-  background(0);
-  // select white as a colour
-  fill(255);
-  // draw a rectangle
-  rect(150, 150, 100, 100);
-}
+
+maxBeats = testData[Object.keys(testData)[0]].sharePrice.length;
+
+var mainLoop = new Tone.Clock(function() {
+  for (var i = 0; i < companies.length; i++) {
+    companies[i].makeAndPlay(beat);
+  }
+
+  beat++;
+
+  if (beat >= maxBeats) {
+    mainLoop.stop();
+  } 
+}, 4);
+
+mainLoop.start();
+
+// function setup() {
+//   // create a canvas
+//   createCanvas(600, 800);
+
+//   console.log(makeNoteValues(1, 0))
+// }
+
+// function draw() {
+//   // colour the background black
+//   background(0);
+//   // select white as a colour
+//   fill(255);
+//   // draw a rectangle
+//   rect(150, 150, 100, 100);
+// }
