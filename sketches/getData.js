8530eabb96499ad394620fc18dcbc0fe2fadeff2
Horatio3 <gamingmonkey2287@gmail.com>
2019-08-06 15:47:19 +0100

    feat: add get data

---
+++ sketches/getData.js
@@ -0,0 +1,65 @@
+    // VARIABLES
+
+    // Constants
+
+    const API_KEY = "8KOBOUBTEF7RHR21";
+
+    // FUNCTIONS
+
+    async function getExchangeRate(from, to) {
+        const requestURL = `https://www.alphavantage.co/query?function=FX_MONTHLY&from_symbol=${from}&to_symbol=${to}&apikey=${API_KEY}`;
+
+        // You can only do 5 calls per minute
+
+        const response = await fetch(requestURL);
+        const data = await response.json();
+
+        const dataValuesArray = Object.entries(data["Time Series FX (Monthly)"])
+            .sort(function (a, b) {
+                return new Date(a[0]) - new Date(b[0]);
+            })
+            .map(function (item) {
+                return parseFloat(item[1]["4. close"]);
+            });
+
+        return dataValuesArray;
+    }
+
+    async function getStonks(symbol) {
+        const requestURL = `https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=${symbol}&apikey=${API_KEY}`;
+
+        const response = await fetch(requestURL);
+        const data = await response.json();
+
+        const sortedEntries = Object.entries(data["Monthly Time Series"])
+            .sort(function (a, b) {
+                return new Date(a[0]) - new Date(b[0]);
+            });
+
+        const sharePriceArray = sortedEntries
+            .map(function (item) {
+                return parseFloat(item[1]["4. close"])
+            });
+
+        const volumeArray = sortedEntries
+            .map(function (item) {
+                return parseFloat(item[1]["5. volume"])
+            });
+
+        return {
+            volume: volumeArray,
+            sharePrice: sharePriceArray
+        };
+    }
+
+    // call this with async / await
+    async function getData() {
+        return {
+            google: await getStonks("GOOG"),
+            microsoft: await getStonks("MSFT"),
+            apple: await getStonks("AAPL"),
+            wetherspoons: await getStonks("JDW.LON"),
+            GBPtoUSD: await getExchangeRate("GBP", "USD"),
+            //GBPtoEUR: await getExchangeRate("GBP", "EUR")
+        };
+    };
--- sketches/sketch.js
+++ sketches/sketch.js
@@ -1,30 +1,38 @@
-const testData = {
-  "companyA": {
+// const testData = {
+//   "companyA": {
 
-    'sharePrice': [67, 70, 76, 56, 45, 40, 32, 25],
-    'volume': [45, 50, 56, 60, 62, 62, 61, 58]
-  },
+//     'sharePrice': [67, 70, 76, 56, 45, 40, 32, 25],
+//     'volume': [45, 50, 56, 60, 62, 62, 61, 58]
+//   },
 
-  "companyB": {
+//   "companyB": {
 
-    'sharePrice': [67, 70, 76, 56, 45, 40, 32, 25],
-    'volume': [45, 50, 56, 60, 62, 62, 61, 58]
-  },
+//     'sharePrice': [67, 70, 76, 56, 45, 40, 32, 25],
+//     'volume': [45, 50, 56, 60, 62, 62, 61, 58]
+//   },
 
-  "companyC": {
-    'sharePrice': [67, 70, 76, 56, 45, 40, 32, 25],
-    'volume': [45, 50, 56, 60, 62, 62, 61, 58]
-  },
+//   "companyC": {
+//     'sharePrice': [67, 70, 76, 56, 45, 40, 32, 25],
+//     'volume': [45, 50, 56, 60, 62, 62, 61, 58]
+//   },
 
-  "companyD": {
-    'sharePrice': [67, 70, 76, 56, 45, 40, 32, 25],
-    'volume': [45, 50, 56, 60, 62, 62, 61, 58]
-  },
+//   "companyD": {
+//     'sharePrice': [67, 70, 76, 56, 45, 40, 32, 25],
+//     'volume': [45, 50, 56, 60, 62, 62, 61, 58]
+//   },
 
-  "ukInterestRate": [0.67, 0.70, 0.76, 0.56, 0.45, 0.40, 0.32, 0.25],
-  "GBPtoUSD": [1.67, 1.70, 1.76, 1.56, 1.45, 1.40, 1.32, 1.25]
+//   "ukInterestRate": [0.67, 0.70, 0.76, 0.56, 0.45, 0.40, 0.32, 0.25],
+//   "GBPtoUSD": [1.67, 1.70, 1.76, 1.56, 1.45, 1.40, 1.32, 1.25]
 
-}
+// }
+
+
+// async function getAsyncData() {
+//   const data = await getData();
+//   console.log(data);
+// }
+
+// getAsyncData();
 
 
 const dummyArray = [60, 0.8];
@@ -34,11 +42,17 @@ class Company {
     var synth = new Tone.Synth().toMaster();
   }
 
-  makeNoteValues(company, index) {
-    var volume = Object.values(testData)[company].volume[index]
-    var sharePrice = Object.values(testData)[company].sharePrice[index]
+  async makeNoteValues(company, index) {
+    const testData = await getData();
+
+    console.log(testData)
+
+    var volume = testData[company].volume[index]
+    var sharePrice = testData[company].sharePrice[index]
 
     var currentValues = [volume, sharePrice]
+
+    console.log(currentValues)
 
     return currentValues
   }
@@ -49,22 +63,15 @@ class Company {
   }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-function setup() {
+async function setup() {
   // create a canvas
   createCanvas(600, 800);
 
-  console.log(makeNoteValues(1, 0))
+  const companyA = new Company()
+
+  const companyNotes = await companyA.makeNoteValues('apple', 0)
+
+  console.log(companyNotes)
 }
 
 function draw() {
